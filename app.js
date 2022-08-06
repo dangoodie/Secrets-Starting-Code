4cf7f79f91986eb1b57a21b2f10ad3aa0ea5273f
dangoodie <70499319+dangoodie@users.noreply.github.com>
2022-08-06 21:22:21 +1000

    salting passwords integration

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const hash = require("hash.js");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,39 +35,38 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: hash.sha256().update(req.body.password).digest("hex"),
-  });
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
 
-  newUser.save((err) => {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      res.send(err);
-      console.log(err);
-    }
+    newUser.save((err) => {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        res.send(err);
+        console.log(err);
+      }
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = hash.sha256().update(req.body.password).digest("hex");
+  const password = req.body.password;
 
   User.findOne({ email: username }, (err, foundUser) => {
-    if (!err) {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.send("Wrong password");
-        }
-      } else {
-        res.send("No user found");
-      }
-    } else {
+    if (err) {
       console.log(err);
-      res.send(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      }
     }
   });
 });
