ce7075b4ad88d018d6586cbd3649245c38111242
dangoodie <70499319+dangoodie@users.noreply.github.com>
2022-08-07 01:46:36 +1000

    still trying to fix facebook authentication

--- app.js
+++ app.js
@@ -80,7 +80,8 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "https://blooming-depths-46268.herokuapp.com/auth/facebook/secrets",
+      callbackURL:
+        "https://blooming-depths-46268.herokuapp.com/auth/facebook/secrets",
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
